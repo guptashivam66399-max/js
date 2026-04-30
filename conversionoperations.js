5a09caf5b2e32b5b22a6e281afb0d93c216b449a
Shivam-2917 <shivamgupta291105@gmail.com>
2026-04-30 11:46:13 +0530

    conversion

---
+++ conversionoperations.js
@@ -0,0 +1,27 @@
+// Conversion Operations in JavaScript
+// let score ='';
+// console.log("The score is: " + score);
+// console.log(typeof score); 
+
+//number
+// let valueInNumber = Number(score);
+// console.log("The value in number is: " + valueInNumber);
+// console.log(typeof valueInNumber);
+
+//boolean
+// let booleanValue = Boolean(score);
+// console.log(booleanValue);
+// console.log(typeof booleanValue);
+
+//string
+// let stringValue = String(score);
+// console.log(stringValue);
+// console.log(typeof stringValue);
+
+//conversion
+// console.log("1" + 2); // "12"
+// console.log(1 + "2"); // "12"
+// console.log("1"+2 +2); // "122"
+// console.log(1 + 2 + "2"); // "32"
+// note:1)if string is first then it will perform concatenation
+// 2)if number is first then it will perform addition than concatenation
