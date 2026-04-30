69a9a70d1a5c12ed9edea15325789a2a6fd539e0
Shivam-2917 <shivamgupta291105@gmail.com>
2026-04-30 11:17:01 +0530

    first commit

---
+++ variablesandconstant.js
@@ -0,0 +1,7 @@
+const PI = 3.14159;
+let score = 33;
+let name = "John Doe";
+// console.log("The value of PI is: " + PI);
+// console.log("The score is: " + score);
+// console.log("The name is: " + name);
+console.table({ PI, score, name });
