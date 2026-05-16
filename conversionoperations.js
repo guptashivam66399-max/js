f0b74e9f0dd944404e07c2fd1566d01ad367b538
Shivam-2917 <shivamgupta291105@gmail.com>
2026-05-16 13:02:43 +0530

    conversion and conditional operaters

--- conversionoperations.js
+++ conversionoperations.js
@@ -25,3 +25,33 @@
 // console.log(1 + 2 + "2"); // "32"
 // note:1)if string is first then it will perform concatenation
 // 2)if number is first then it will perform addition than concatenation
+
+// *********************** Operations ***********************
+
+// let value =3
+// let negvalue= -value
+// // console.log(negvalue)
+// console.log(4**3); 
+// console.log(2/3);
+// console.log(2%3);
+
+// let gameCounter = 100;
+// ++gameCounter; // 101
+// console.log(gameCounter);
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+// console.log(num1);
+// console.log(num3);
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1); its because of the string is converted to number and then comparison is done so "2" is converted to 2 and "02" is converted to 2 and then comparison is done
+    
