7da7c038e95d478cc511230d984dcef9b3d4613b
Shivam-2917 <shivamgupta291105@gmail.com>
2026-05-20 18:47:42 +0530

    strings methods

---
+++ string.js
@@ -0,0 +1,29 @@
+// const name = "hitesh"
+// const repoCount = 50
+
+// // console.log(name + repoCount + " Value");
+// // console.log(`my name is ${name} my marks is ${repoCount}`)
+
+const cust =new String("Shiiv@max-gupta")
+
+// console.log(cust[2]);
+// console.log(cust.__proto__);
+
+// console.log(cust.replace("iv-","vam"));
+// console.log(cust.length);
+// console.log(cust.slice(3,8));
+// console.log(cust.charAt(8));
+// console.log(cust.includes("max"));
+
+// const newstring= cust.slice(5,9)
+// console.log(newstring)
+
+// const anotherstring= cust.substring(9,15)
+// console.log(anotherstring);
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(cust.split('@'));
+
+
+
+
